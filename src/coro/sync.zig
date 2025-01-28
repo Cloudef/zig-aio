2fcabb398ed24a540a3e2e9159bab682f1e61011
YiraSan <yirasan.bk@gmail.com>
2025-01-28 13:02:01 +0100

    implement async Mutex and RwLock

--- src/coro/sync.zig
+++ src/coro/sync.zig
@@ -71,3 +71,270 @@ pub const ResetEvent = struct {
         self.is_set = false;
     }
 };
+
+const std = @import("std");
+
+const coro = @import("../coro.zig");
+const aio = @import("aio");
+
+/// A thread-safe mutual exclusion between schedulers.
+const Mutex = struct {
+    native: std.Thread.Mutex,
+    event_source: aio.EventSource,
+
+    pub fn init() !@This() {
+        return .{
+            .native = .{},
+            .event_source = try .init(),
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.event_source.deinit();
+    }
+
+    pub fn lock(self: *@This()) !void {
+        if (Frame.current()) |frame| {
+            if (frame.canceled) return error.Canceled;
+
+            while (!self.native.tryLock()) {
+                try coro.io.single(.wait_event_source, .{
+                    .source = &self.event_source,
+                });
+
+                if (frame.canceled) return error.Canceled;
+            }
+        } else {
+            while (!self.native.tryLock()) {
+                self.event_source.wait();
+            }
+        }
+    }
+
+    pub fn unlock(self: *@This()) void {
+        self.native.unlock();
+        self.event_source.notify();
+    }
+};
+
+/// A thread-safe read-write lock between schedulers.
+pub const RwLock = struct {
+    event_source: aio.EventSource,
+    guard: std.Thread.Mutex,
+    locked: bool,
+    counter: usize,
+
+    pub fn init() !@This() {
+        return .{
+            .event_source = try .init(),
+            .guard = .{},
+            .locked = false,
+            .counter = 0,
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.event_source.deinit();
+    }
+
+    fn tryLock(self: *@This()) bool {
+        self.guard.lock();
+        defer self.guard.unlock();
+
+        if (!self.locked) {
+            self.locked = true;
+            self.counter = 0;
+            return true;
+        }
+
+        return false;
+    }
+
+    fn tryLockShared(self: *@This()) bool {
+        self.guard.lock();
+        defer self.guard.unlock();
+
+        if (self.locked) {
+            if (self.counter > 0) {
+                self.counter += 1;
+                return true;
+            }
+        } else {
+            self.locked = true;
+            self.counter = 1;
+            return true;
+        }
+
+        return false;
+    }
+
+    pub fn lock(self: *@This()) !void {
+        if (Frame.current()) |frame| {
+            if (frame.canceled) return error.Canceled;
+
+            while (!self.tryLock()) {
+                try coro.io.single(.wait_event_source, .{
+                    .source = &self.event_source,
+                });
+
+                if (frame.canceled) return error.Canceled;
+            }
+        } else {
+            while (!self.tryLock()) {
+                self.event_source.wait();
+            }
+        }
+    }
+
+    pub fn lockShared(self: *@This()) !void {
+        if (Frame.current()) |frame| {
+            if (frame.canceled) return error.Canceled;
+
+            defer self.event_source.notify();
+
+            while (!self.tryLockShared()) {
+                try coro.io.single(.wait_event_source, .{
+                    .source = &self.event_source,
+                });
+
+                if (frame.canceled) return error.Canceled;
+            }
+        } else {
+            defer self.event_source.notify();
+
+            while (!self.tryLockShared()) {
+                self.event_source.wait();
+            }
+        }
+    }
+
+    pub fn unlock(self: *@This()) void {
+        self.guard.lock();
+        defer self.event_source.notify();
+        defer self.guard.unlock();
+
+        if (self.counter > 0) {
+            self.counter -= 1;
+            if (self.counter != 0) {
+                return;
+            }
+        }
+
+        self.locked = false;
+    }
+};
+
+test "Mutex" {
+    const Test = struct {
+        fn incrementer(lock: *Mutex, value: *usize) !void {
+            try lock.lock();
+            defer lock.unlock();
+
+            const stored = value.*;
+
+            // simulates a "workload"
+            try coro.io.single(.timeout, .{ .ns = std.time.ns_per_ms });
+
+            value.* = stored + 1000;
+        }
+
+        fn test_thread(lock: *Mutex, value: *usize) !void {
+            var scheduler = try coro.Scheduler.init(std.testing.allocator, .{});
+            defer scheduler.deinit();
+
+            for (0..128) |_| {
+                const task = try scheduler.spawn(incrementer, .{ lock, value }, .{});
+                task.detach();
+            }
+
+            try scheduler.run(.wait);
+        }
+    };
+
+    var lock: Mutex = try .init();
+    defer lock.deinit();
+
+    var value: usize = 0;
+
+    var threads: [8]std.Thread = undefined;
+
+    for (0..8) |i| {
+        threads[i] = try std.Thread.spawn(.{}, Test.test_thread, .{ &lock, &value });
+    }
+
+    for (threads) |thread| {
+        thread.join();
+    }
+
+    std.debug.assert(value == 1024000);
+}
+
+test "RwLock" {
+    const Test = struct {
+        fn incrementer(lock: *RwLock, value: *usize, value2: *usize) !void {
+            try lock.lock();
+            defer lock.unlock();
+
+            value.* += 1000;
+
+            const stored = value2.*;
+
+            // simulates a "workload" + makes coroutines to try lock
+            try coro.io.single(.timeout, .{ .ns = std.time.ns_per_ms });
+
+            value2.* = stored + 1000;
+        }
+
+        fn checker(lock: *RwLock, value: *usize, value2: *usize) !void {
+            while (true) {
+                // simulates a "workload"
+                try coro.io.single(.timeout, .{ .ns = std.time.ns_per_ms });
+
+                try lock.lockShared();
+                defer lock.unlock();
+
+                if (value.* == 1024000 and value2.* == 1024000) break;
+            }
+        }
+
+        fn test_thread(lock: *RwLock, value: *usize, value2: *usize) !void {
+            var scheduler = try coro.Scheduler.init(std.testing.allocator, .{});
+            defer scheduler.deinit();
+
+            for (0..128) |_| {
+                const task = try scheduler.spawn(incrementer, .{ lock, value, value2 }, .{});
+                task.detach();
+            }
+
+            for (0..16) |_| {
+                const task = try scheduler.spawn(checker, .{ lock, value, value2 }, .{});
+                task.detach();
+            }
+
+            try scheduler.run(.wait);
+        }
+    };
+
+    var lock: RwLock = try .init();
+    defer lock.deinit();
+
+    var value: usize = 0;
+    var value2: usize = 0;
+
+    var threads: [8]std.Thread = undefined;
+
+    for (0..8) |i| {
+        threads[i] = try std.Thread.spawn(.{}, Test.test_thread, .{ &lock, &value, &value2 });
+    }
+
+    for (threads) |thread| {
+        thread.join();
+    }
+
+    std.debug.assert(value == 1024000);
+    std.debug.assert(value2 == 1024000);
+
+    // check if it has successfully returned in its initial state.
+    std.debug.assert(lock.counter == 0);
+    std.debug.assert(lock.locked == false);
+}
