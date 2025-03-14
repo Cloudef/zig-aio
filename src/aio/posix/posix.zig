8fb06107924fee8366cf1b6f11efbcb1861154be
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2025-03-15 08:42:35 +0900

    aio: add error.OperationNotSupported to shared error set

--- src/aio/posix/posix.zig
+++ src/aio/posix/posix.zig
@@ -273,7 +273,7 @@ pub fn perform(comptime op_type: Operation, op: Operation.map.getAssertContains(
         .notify_event_source => op.source.notify(),
         .wait_event_source => try op.source.waitNonBlocking(),
         .close_event_source => op.source.deinit(),
-        .splice => @panic("fixme: implement splice for posix"),
+        .splice => return error.OperationNotSupported, // FIXME
         // these must be implemented by the backend
         .nop, .timeout, .link_timeout, .cancel, .poll => unreachable,
     }
